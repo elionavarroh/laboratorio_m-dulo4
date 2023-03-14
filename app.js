1d2db796b5a54faac5b59cf5e770b769abec3f64
elionavarroh <elionavarroh@gmail.com>
2023-03-14 15:49:25 +0100

    change the input and button iside of the html

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-//function  sum(numberA, numberB) {
-//    var result = numberA + numberB;
-//    return result;
-//    console.log("Tu resulato es" + result);
-//}
+//INPUTS
+var numberA = () => parseInt(document.getElementById(input-number1).value);
+var numberB = () => parseInt(document.getElementById(input-number2).value);
+
+//FUNCIONES DE LAS OPERACIONES
+var sum = (numberA, numberB) => numberA() + numberB();
+
+var rest = (numberA, numberB) => numberA() - numberB();
+
+var multiply = (numberA, numberB) => numberA() * numberB();
+
+var dividy = (numberA, numberB) => numberA() / numberB();
+
+//RESULTADOS
+var showresultsum = () => document.getElementById("result").innerText = sum();
+
+var showresultrest = () => document.getElementById("result").innerText = rest();
+
+var showresultmultiply = () => document.getElementById("result").innerText = multiply();
+
+var showresultdividy = () => document.getElementById("result").innerText = dividy();
+
+//EVENTOS
+var handleinputchange = (event) => {
+    document.getElementById("number1").addEventListener("click", handleinputchange);
+}
 
 var handleinputchange = (event) => {
-    alert(event.target.value);
+    document.getElementById("number2").addEventListener("click", handleinputchange);
 }
-document.getElementById("buttonleft").addEventListener("change", handleinputchange);
 
+var handleButtonClick = () => {
+    document.getElementById("sum").addEventListener("click", handleButtonClick);
+}
 
-var sum = (numberA, numberB) => numberA + numberB
-console.log(sum)
+var handleButtonClick = () => {
+    document.getElementById("rest").addEventListener("click", handleButtonClick);
+}
 
+var handleButtonClick = () => {
+    document.getElementById("multiply").addEventListener("click", handleButtonClick);
+}
 
+var handleButtonClick = () => {
+    document.getElementById("dividy").addEventListener("click", handleButtonClick);
+}
